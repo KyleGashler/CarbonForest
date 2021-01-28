777fe069fe632926924e1c2c3a51e871061a6910
Kyle Gashler <kgashler@newrelic.com>
2021-01-27 18:59:45 -0700

    first cloud function fetching shopify data

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+const functions = require("firebase-functions");
+const fetch = require('node-fetch');
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.getShopData = functions.https.onRequest(async (req, res) => {
+    // // Grab the text parameter.
+    // const original = req.query.text;
+    // Push the new message into Firestore using the Firebase Admin SDK.
+    // const writeResult = await admin.firestore().collection('messages').add({original: original});
+    // Send back a message that we've successfully written the message
+
+    const response = await fetch('https://447c649ad8e1d0d93d6daf2fde37e9bf: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@carbonforest.myshopify.com/admin/api/2021-01/customers.json')
+    const resData = await response.json()
+
+    res.json({ ...resData });
+});
