76b9ba5d6b26acb4426ac18667513a66da8f9bd2
Kyle Gashler <kgashler@newrelic.com>
2021-02-04 09:01:45 -0700

    front end login

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
 import logo from './images/thumb-carbon.png';
 import './App.css';
 import Header from './components/Header';
+import Login from './views/login';
 
+const userEmail = "";
 
 function App() {
   return (
-    <div className="App">
-
-      <Header />
-
-      <header className="App-header">
-        <body>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            CARBON FOREST FOREVER!!!
-        </p>
-          <a
-            className="App-link"
-            href="https://carbonforest.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            CarbonForest
-        </a>
-        </body>
-      </header>
-
-    </div>
+    switchOnLogin()
   );
+}
+
+function switchOnLogin() {
+  if (userEmail) {
+    return (
+      <div className="App">
+        <Header />
+        <header className="App-header">
+          <body>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              CARBON FOREST FOREVER!!!
+        </p>
+            <a
+              className="App-link"
+              href="https://carbonforest.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              CarbonForest
+        </a>
+          </body>
+        </header>
+      </div>
+
+    )
+  } else {
+    return (
+      <div>
+        <Header />
+        <Login />
+      </div>
+    )
+  }
 }
 
 export default App;
