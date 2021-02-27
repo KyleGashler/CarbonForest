e8c861952f939d8910c2d2e2f7d73be00a903913
Kyle Gashler <kgashler@newrelic.com>
2021-02-26 18:23:06 -0700

    first pannel done-ish

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { SocialIcon } from 'react-social-icons';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
 import dashboardBackground from '../images/dashboardBackground.png'; // Import using relative path
 
@@ -30,7 +31,7 @@ const useStyles = makeStyles((theme) => ({
         lineHeight: "1%"
     },
     titles: {
-        paddingTop: "218px",
+        paddingTop: "205px",
     },
     welcome: {
         fontSize: "60pt",
@@ -55,6 +56,9 @@ const useStyles = makeStyles((theme) => ({
         width: "75%",
         background: "white",
         height: "4px",
+    },
+    share: {
+        paddingTop: "120px"
     }
 }));
 
@@ -79,8 +83,13 @@ const Dashboard = () => {
                                         <hr className={classes.separator}></hr>
                                         <p>Trees planted</p>
                                         <p>to date.</p>
-                                        <p>SHARE YOUR PROGRESS</p>
-                                        <span></span>
+                                        <p className={classes.share}>SHARE YOUR PROGRESS</p>
+                                        <SocialIcon url="https://www.facebook.com/sharer/sharer.php?u=carbonforest.org" network="facebook" bgColor="white" />
+                                        <SocialIcon url="https://twitter.com/intent/tweet?url=carbonforest.org&text=" network="twitter" bgColor="white" />
+                                        <SocialIcon url="https://www.instagram.com/carbon_forest/?hl=en" network="instagram" bgColor="white" />
+                                        <SocialIcon url="mailto:caitlin@carbonforest.org?&subject=&cc=&bcc=&body=carbonforest.org%0A" network="email" bgColor="white" />
+                                        <p className={classes.share}>scroll for more <ArrowDropDownIcon></ArrowDropDownIcon> </p>
+
                                     </div>
                                 </Grid>
                                 <Grid item xs={12} sm={4}>
@@ -97,6 +106,7 @@ const Dashboard = () => {
                                         <hr className={classes.separator}></hr>
                                         <p>Of your lifetime carbon</p>
                                         <p>footprint erased.</p>
+
                                     </div>
                                 </Grid>
                             </Grid>
