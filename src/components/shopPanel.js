302b8c95e34174f62c588a67329eac9b23cc66f3
Kyle Gashler <kgashler@newrelic.com>
2021-04-08 09:08:58 -0600

    shop panel progress

--- src/components/shopPanel.js
+++ src/components/shopPanel.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import { withStyles } from '@material-ui/core/styles';
 
 import hat from '../images/hatCircle.png';
 
@@ -17,26 +19,49 @@ const useStyles = makeStyles({
         textAlign: "left",
         paddingLeft: "20px",
         paddingRight: "20px",
-        fontSize: "50pt",
         color: "black",
-        height: "600px",
+        height: "650px",
         fontWeight: "bold",
-        fontFamily: "Chivo, sans-serif"
-    },
-    heading: {
-        textAlign: "left",
+        fontFamily: "Chivo, sans-serif",
+        marginBottom: "50px"
     },
     hat: {
         backgroundImage: `url(${hat})`,
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center center",
         backgroundSize: "500px",
-        height: 500,
+        height: 400,
+        paddingTop: "55px",
+
     },
-    products: {
-        alignContent: "left"
+    invite: {
+        paddingTop: "25px",
+        paddingBottom: "50px",
+        color: "white",
+        backgroundColor: "#E6600E",
+    },
+    saving: {
+        marginRight: "400px",
+        marginLeft: "400px",
+    },
+    productDetail: {
+        textAlign: "center"
+    },
+    title: {
+        fontSize: "50pt"
     }
 });
+
+const ColorButton = withStyles((theme) => ({
+    root: {
+        color: "#E6600E",
+        backgroundColor: "white",
+        '&:hover': {
+            backgroundColor: "gray",
+        },
+
+    },
+}))(Button);
 
 export default function ShopPanel() {
     const classes = useStyles();
@@ -45,31 +70,62 @@ export default function ShopPanel() {
 
         <Grid item xs={12} sm={12}>
             <div className={classes.shop}>
-                <p style={{ marginBottom: "0" }} className={classes.heading}>Shop CarbonForest.</p>
+                <p style={{ marginBottom: "0" }} className={classes.title}>Shop CarbonForest.</p>
                 <hr className={classes.separator2}></hr>
                 <Grid
                     container
-                    className={classes.products}
                     spacing={3}
                     justify="center"
                     alignItems="center"
                 >
                     <Grid item >
-                        <ArrowBackIosIcon></ArrowBackIosIcon>
+                        <ArrowBackIosIcon
+                            style={{ color: "#E6600E" }}
+                            fontSize="large"
+                        >
+
+                        </ArrowBackIosIcon>
                     </Grid>
                     <Grid item xs={3}>
                         <div className={classes.hat}></div>
+                        <div className={classes.productDetail}>
+                            <p>Carbon Forest Hat</p>
+                            <p>$14</p>
+                        </div>
                     </Grid>
                     <Grid item xs={3}>
                         <div className={classes.hat}></div>
+                        <div className={classes.productDetail}>
+                            <p>Carbon Forest Hat</p>
+                            <p>$14</p>
+                        </div>
                     </Grid>
                     <Grid item xs={3}>
                         <div className={classes.hat}></div>
+                        <div className={classes.productDetail}>
+                            <p>Carbon Forest Hat</p>
+                            <p>$14</p>
+                        </div>
                     </Grid>
                     <Grid item >
-                        <ArrowForwardIosIcon></ArrowForwardIosIcon>
+                        <ArrowForwardIosIcon
+                            style={{ color: "#E6600E" }}
+                            fontSize="large"
+                        >
+
+                        </ArrowForwardIosIcon>
                     </Grid>
                 </Grid>
+
+            </div>
+            <div className={classes.invite}>
+                <h1 className={classes.saving}>
+                    SAVING THE WORLD IS BETTER WITH FRIENDS!
+                    INVITE YOUR FRIENDS TO JOIN CARBONFOREST.
+                </h1>
+                <ColorButton>
+                    REFER A FRIEND
+                </ColorButton>
             </div>
         </Grid>
 
