4462bfc4d50921dc0fd332ab98f9bd25d6ef264b
Kyle Gashler <kgashler@newrelic.com>
2021-04-16 12:12:14 -0600

    image updates for products

--- src/components/shopPanel.js
+++ src/components/shopPanel.js
@@ -6,7 +6,10 @@ import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import { withStyles } from '@material-ui/core/styles';
 
+
 import hat from '../images/hatCircle.png';
+import sweat from '../images/sweatshirt.png';
+import tee from '../images/tee.png';
 
 const useStyles = makeStyles({
     separator2: {
@@ -27,6 +30,23 @@ const useStyles = makeStyles({
     },
     hat: {
         backgroundImage: `url(${hat})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+        backgroundSize: "550px",
+        height: 420,
+        paddingTop: "55px",
+    },
+    tee: {
+        backgroundImage: `url(${tee})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+        backgroundSize: "500px",
+        height: 400,
+        paddingTop: "55px",
+
+    },
+    sweat: {
+        backgroundImage: `url(${sweat})`,
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center center",
         backgroundSize: "500px",
@@ -87,13 +107,13 @@ export default function ShopPanel() {
                         </ArrowBackIosIcon>
                     </Grid>
                     <Grid item xs={3}>
-                        <a href="https://carbonforest.org/products/carbon-forest-hat?_pos=1&_sid=100118769&_ss=r">
-                            <div className={classes.hat}></div>
+                        <a href="https://carbonforest.org/products/carbonforest-eco-hoodie?pr_prod_strat=collection_fallback&pr_rec_pid=6588900311234&pr_ref_pid=6583387717826&pr_seq=uniform">
+                            <div className={classes.sweat}></div>
                         </a>
 
                         <div className={classes.productDetail}>
-                            <p>Carbon Forest Hat</p>
-                            <p>$14</p>
+                            <p>CarbonForest Eco Hoodie</p>
+                            <p>$48</p>
                         </div>
                     </Grid>
                     <Grid item xs={3}>
@@ -101,17 +121,17 @@ export default function ShopPanel() {
                             <div className={classes.hat}></div>
                         </a>
                         <div className={classes.productDetail}>
-                            <p>Carbon Forest Hat</p>
+                            <p>CarbonForest Econnscious Hat</p>
                             <p>$14</p>
                         </div>
                     </Grid>
                     <Grid item xs={3}>
-                        <a href="https://carbonforest.org/products/carbon-forest-hat?_pos=1&_sid=100118769&_ss=r">
-                            <div className={classes.hat}></div>
+                        <a href="https://carbonforest.org/products/carbonforest-eco-triblend-tee?_pos=1&_sid=5dca28cab&_ss=r">
+                            <div className={classes.tee}></div>
                         </a>
                         <div className={classes.productDetail}>
-                            <p>Carbon Forest Hat</p>
-                            <p>$14</p>
+                            <p>CarbonForest Triblend Tee</p>
+                            <p>$20</p>
                         </div>
                     </Grid>
                     <Grid item >
@@ -129,6 +149,8 @@ export default function ShopPanel() {
                     SAVING THE WORLD IS BETTER WITH FRIENDS!
                     INVITE YOUR FRIENDS TO JOIN CARBONFOREST.
                 </h1>
+                <input id="refer" type="text"></input>
+                <br></br>
                 <ColorButton>
                     REFER A FRIEND
                 </ColorButton>
