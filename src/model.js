3d0b4258dc73144986495e988bca5a1e3eb66349
Kyle Gashler <kgashler@newrelic.com>
2021-05-06 20:42:43 -0600

    customer count and tree location data

--- src/model.js
+++ src/model.js
@@ -1,33 +1,34 @@
-import { action, thunk } from 'easy-peasy';
+import { action, thunk } from "easy-peasy";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    userEmail: "",
-    customer: {},
-    error: "",
-    requestInFlight: false,
-    // Actions
-    loadCustomerState: action((state, data) => {
-        state.customer = data;
-    }),
-    addEmailToStore: action((state, email) => {
-        if (email) {
-            state.userEmail = email.trim().toLowerCase();
-        }
-    }),
-    toggleRequestInFlight: action((state, val) => {
-        state.requestInFlight = val;
-    }),
-    //thunks
-    saveCustInfo: thunk(async (actions, userEmail) => {
-        actions.toggleRequestInFlight(true);
+  userEmail: "",
+  customer: {},
+  error: "",
+  requestInFlight: false,
+  // Actions
+  loadCustomerState: action((state, data) => {
+    state.customer = data;
+  }),
+  addEmailToStore: action((state, email) => {
+    if (email) {
+      state.userEmail = email.trim().toLowerCase();
+    }
+  }),
+  toggleRequestInFlight: action((state, val) => {
+    state.requestInFlight = val;
+  }),
+  //thunks
+  saveCustInfo: thunk(async (actions, userEmail) => {
+    actions.toggleRequestInFlight(true);
 
-        const encodedEmail = encodeURIComponent(userEmail);
-        console.log("encodedEmail", encodedEmail);
-        const response = await fetch(`https://profile.carbonforest.org/appData?email=${encodedEmail}`);
-        const data = await response.json();
+    const encodedEmail = encodeURIComponent(userEmail);
+    const response = await fetch(
+      `https://profile.carbonforest.org/appData?email=${encodedEmail}`
+    );
+    const data = await response.json();
 
-        actions.loadCustomerState(data);
-        actions.toggleRequestInFlight(false);
-    }),
-}
+    actions.loadCustomerState(data);
+    actions.toggleRequestInFlight(false);
+  }),
+};
